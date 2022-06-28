b83eb62f33213195b8fe58d79c6a254a0587ea17
irakli9909 <razora.irakli@gmail.com>
2022-06-28 13:44:58 +0400

    add error page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/Pages/Home';
 import About from './Components/Pages/About';
 import Contact from './Components/Pages/Contact';
 import Projects from './Components/Pages/Projects';
+import ErrorPage from './Components/Pages/ErrorPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/projects' element={<Projects />} />
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
     </Router>
   );
